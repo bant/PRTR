141576a4c8124c0ec50fbd67b8d06c29b999788c
bant <bant62@gmail.com>
2018-05-13 15:30:26 +0900

    javascriptの修正2

--- public/js/tools.js
+++ public/js/tools.js
@@ -4,83 +4,122 @@ jQuery( function( $ ) {
   	var status = $('.display', this).text();
   	if (status == '非表示にする') {
   	  $(this).parent().find('table').hide();
-  	  $(this).parent().find('.graph').hide();
   	  $('.display', this).text('表示する');
     } else {
   	  $(this).parent().find('table').show();
-  	  $(this).parent().find('.graph').show();
   	  $('.display', this).text('非表示にする');
     }
   });
- 
-  // メニューリンク
-  var mode = 'close'; 
-  $('#menus').addClass('menuClose').append("<i class='fa fa-4x fa-chevron-circle-left' aria-hidden='true' id='arrow' title='メニューを開く'></i><div id='linkMenus'></div>").on('click', function(){
-  	menuMove(mode);
-  });
-  $('#linkMenus').append("<button id='linkCompany' class='btn btn-default btn-sm'>事業者検索</button><br><button id='linkFactory' class='btn btn-default btn-sm'>事業所検索</button><br><button id='linkBusinessType' class='btn btn-default btn-sm'>業種別比較</button><br><button id='linkTransporter' class='btn btn-default btn-sm'>輸送者別比較</button><br><button id='linkPref' class='btn btn-default btn-sm'>都道府県別比較</button><br><button id='linkGas' class='btn btn-default btn-sm'>温室効果ガス別比較</button><br><button id='linkMenu' class='btn btn-default btn-sm'>メニュー</button><br><button id='pageUp' class='btn btn-default btn-sm'>ページトップへ</button>").css({
-  	  height: '100%',
-  	  width: '0rem'
-  	});
 
-  function menuMove(m) {
-  	switch (m) {
-  	  case 'close':
-  	  	$('#menus').animate({
-  	  	  bottom: '0.1rem',
-  	  	  right: '1rem'
-  	  	}, 200, 'swing', function(){
-  	  	  $('#linkMenus').animate({
-  	  	  	height: '100%',
-  	  	  	width: '20rem'
-  	  	  }, 200, 'swing', function(){
-  	  	    $("#linkMenus .btn-sm").css('width', '20rem');
-  	  	  });
-  	  	  $('#arrow').removeClass('fa-chevron-circle-left').addClass('fa-chevron-circle-right').attr('title', 'メニューを閉じる').css('marginLeft', '-10rem');
-  	  	  mode = 'open';
-  	  	});
-  	    break;
-  	  case 'open':
-  	  	$('#linkMenus').animate({
-  	  	  height: '100%',
-  	  	  width: '0rem'
-  	  	}, 200, 'swing');
-  	  	$('#menus').animate({
-  	  	  bottom: '0.1rem',
-  	  	  right: '1rem'
-  	  	}, 200, 'swing', function(){
-  	  	  $('#arrow').removeClass('fa-chevron-circle-right').addClass('fa-chevron-circle-left').attr('title', 'メニューを開く').css('marginLeft', '0rem');
-  	  	  mode = 'close';
-  	  	});
-   	}
-  }
+ 
 
-  $('#linkCompany').on('click', function(){
-  	location.href = '/search/Company';
+  // メニューリンク
+
+  var mode = 'close'; 
+
+  $('#menus').addClass('menuClose').append("<i class='fa fa-4x fa-chevron-circle-left' aria-hidden='true' id='arrow' title='メニューリンクを開く'></i><div id='linkMenus'></div>").on('click', function(){
+
+  	menuMove(mode);
+
+  });
+
+  $('#linkMenus').append("<button id='linkCompany' class='btn btn-default btn-sm'>事業者検索</button><br><button id='linkFactory' class='btn btn-default btn-sm'>事業所検索</button><br><button id='linkChemical' class='btn btn-default btn-sm'>化学物質検索</button><br><button id='linkCompare' class='btn btn-default btn-sm'>事業所比較</button><br><button id='linkMenu' class='btn btn-default btn-sm'>メニュー</button><br><button id='pageUp' class='btn btn-default btn-sm'>ページトップへ</button>").css({
+
+  	  height: '100%',
+
+  	  width: '0rem'
+
+  	});
+
+
+  function menuMove(m) {
+
+  	switch (m) {
+
+  	  case 'close':
+
+  	  	$('#menus').animate({
+
+  	  	  bottom: '0.1rem',
+
+  	  	  right: '1rem'
+
+  	  	}, 200, 'swing', function(){
+
+  	  	  $('#linkMenus').animate({
+
+  	  	  	height: '100%',
+
+  	  	  	width: '20rem'
+
+  	  	  }, 200, 'swing', function(){
+
+  	  	    $("#linkMenus .btn-sm").css('width', '20rem');
+
+  	  	  });
+
+  	  	  $('#arrow').removeClass('fa-chevron-circle-left').addClass('fa-chevron-circle-right').attr('title', 'メニューリンクを閉じる');
+
+  	  	  mode = 'open';
+
+  	  	});
+
+  	    break;
+
+  	  case 'open':
+
+  	  	$('#linkMenus').animate({
+
+  	  	  height: '100%',
+
+  	  	  width: '0rem'
+
+  	  	}, 200, 'swing');
+
+  	  	$('#menus').animate({
+
+  	  	  bottom: '0.1rem',
+
+  	  	  right: '1rem'
+
+  	  	}, 200, 'swing', function(){
+
+  	  	  $('#arrow').removeClass('fa-chevron-circle-right').addClass('fa-chevron-circle-left').attr('title', 'メニューリンクを開く');
+
+  	  	  mode = 'close';
+
+  	  	});
+
+   	}
+
+  }
+
+
+  $('#linkCompany').on('click', function(){
+
+  	location.href = '/company/search';
   });
   $('#linkFactory').on('click', function(){
-  	location.href = '/search/Factofy';
+  	location.href = '/factory/search';
   });
-  $('#linkBusinessType').on('click', function(){
-  	location.href = '/compare/MajorBusinessType';
+  $('#linkChemical').on('click', function(){
+  	location.href = '/chemical/search';
   });
-  $('#linkTransporter').on('click', function(){
-  	location.href = '/compare/CompanyDivision';
-  });
-  $('#linkPref').on('click', function(){
-  	location.href = '/compare/Pref';
-  });
-  $('#linkGas').on('click', function(){
-  	location.href = '/compare/Gas';
+  $('#linkCompare').on('click', function(){
+  	location.href = '/discharge/search';
   });
   $('#linkMenu').on('click', function(){
-  	location.href = '/';
-  });
-  $('#pageUp').on('click', function(){
-    location.href = '#breadcrumbs';
+
+  	location.href = '/';
+
   });
 
-  //テーブルソーターを適応する
+  $('#pageUp').on('click', function(){
+
+    location.href = '#breadcrumbs';
+  });
+ 
+   //テーブルソーターを適応する
   $('.tablesorter-green').tablesorter();
-  
+
 });
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,18 +10,18 @@
       <section>
         <h2>検索メニュー</h2>
         <div id="menu">
-          <a href="{{url('factory/search')}}">
-            <button class="btn btn-warning btn-block btn-lg">
-              <i class="fa fa-industry" aria-hidden="true"></i>事業所検索
-            </button>
-          </a>
-          <p>事業所名・住所・業種から検索します。</p>
           <a href="{{url('company/search')}}">
             <button class="btn btn-warning btn-block btn-lg">
               <i class="fa fa-building" aria-hidden="true"></i>事業者検索
             </button>
           </a>
           <p>事業者名・住所から検索します。</p>
+          <a href="{{url('factory/search')}}">
+            <button class="btn btn-warning btn-block btn-lg">
+              <i class="fa fa-industry" aria-hidden="true"></i>事業所検索
+            </button>
+          </a>
+          <p>事業所名・住所・業種から検索します。</p>
           <a href="{{url('chemical/search')}}">
             <button class="btn btn-warning btn-block btn-lg">
               <i class="fa fa-flask" aria-hidden="true"></i>化学物質検索
